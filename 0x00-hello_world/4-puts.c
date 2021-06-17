194171c991a0e135152b1acd4deb35af18126a15
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-17 12:34:51 -0700

    This script prints a message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+ *Script prints sentence
+ *without using printf
+ */
+
+int main(void);
+{
+  char mysent[] = "Programming is like building a multilingual puzzle\n";
+  puts (mysent);
+
+  return(0);
+
+}
