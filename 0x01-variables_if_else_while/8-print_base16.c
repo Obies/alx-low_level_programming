5ea18b6912959e945fb64fe365da7d1ea1a521a9
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-18 13:00:58 -0700

    This script prints base 16 numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always 0(success)
+ */
+int main(void)
+{
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+putchar(digit);
+for (digit = 'a'; digit <= 'f'; digit++)
+putchar(digit);
+putchar('\n');
+return (0);
+}
