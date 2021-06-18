613edacae610e83f73bd42d782473c0a7b552afd
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-18 00:57:19 -0700

    This script prints a message with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+ *main-entry point of code
+ *
+ *Return: 0(successful)
+ */
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+
+}
