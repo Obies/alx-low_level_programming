74489c820fb1b0201299ccd2abd94188aea7d5ab
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-18 12:33:43 -0700

    This script prints single digit numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: always 0(success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit < 10; digit++)
+printf("%i", digit);
+putchar('\n');
+return (0);
+}
