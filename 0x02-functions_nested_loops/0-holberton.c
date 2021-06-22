497c7c6ab7a6d96bfc02988d351b65dc39b6c75e
Obies <41453896+Obies@users.noreply.github.com>
2021-06-22 11:29:29 +0200

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,11 +6,7 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 char *s=("Holberton\n");
 while (*s) putchar(*s++);
-=======
-puts("Holberton");
->>>>>>> c17e5b44f234069a76dabadf90fe0c1f4596b3ac
 return (0);
 } 
