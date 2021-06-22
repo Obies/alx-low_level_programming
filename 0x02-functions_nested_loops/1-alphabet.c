d828808d133ff27bf98a7e3adbd6fde5e00b0313
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-22 10:01:20 -0700

    Prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *main - check the code for ALX School students.
+ *
+ *Return: always 0(success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+return (0);
+}
