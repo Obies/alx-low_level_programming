015dd6b1faa5d6245ebd3d750f1abc68dc7b1b09
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-22 11:33:33 -0700

    Prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_alphabet - print the alphabet in lowercase
+ *
+ *return : void
+ */
+
+void print_alphabet(void)
+{
+int c;
+for (c = 97; c <= 122; c++);
+{
+_putchar(c);
+}
+_putchar('\n');
+}
