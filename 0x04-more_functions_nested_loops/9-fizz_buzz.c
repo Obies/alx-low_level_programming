740a676139d86ff8acbd488719ab3d0f4ed16723
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-24 12:56:46 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,26 +6,26 @@
  */
 int main(void)
 {
-  int i;
+int i;
 
-  for (i = 1; i < 100; i++)
-    if (i % 15 == 0)
-    {
-      printf("FizzBuzz");
-    }
-    else if (i % 5 == 0)
-    {
-      printf("Buzz ");
-    }
-    else if (i % 3 == 0)
-    {
-      printf("Fizz ");
-    }
-    else
-    {
-      printf("%d", i);
-    }
-  printf("Buzz");
-  printf("\n");
-  return (0);
+for (i = 1; i < 100; i++)
+if (i % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d", i);
+}
+printf("Buzz");
+printf("\n");
+return (0);
 }
