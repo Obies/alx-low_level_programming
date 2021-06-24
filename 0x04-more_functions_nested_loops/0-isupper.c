e7ad26be8d132930a5629ff8f62f211c2a3df311
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-24 07:27:10 -0700

    Checks for the uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _isupper - shows 1 if the input is uppercase character. 0 for the other cases.
+ *@c: the character in ASCII code
+ *
+ *Return: 1 for uppercase, 0 for lowercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
