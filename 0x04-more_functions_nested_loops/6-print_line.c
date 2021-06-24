e7e8efb25209fde36590cc50bb59994e16318372
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-24 12:05:01 -0700

    Prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_line - draws straight line in the terminal.
+ *@n: integer amount of '_' to be printed
+ *
+ *Return: no return
+ */
+
+void print_line(void)
+{
+char ch;
+for (ch = 0; ch < n; ch++)
+_putchar('_');
+_putchar('\n');
+}
