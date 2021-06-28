a3f94dfc07d7492a751efdab7b99f51f494cd346
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-28 16:19:07 -0700

    Reverses engineers string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses string
+ * @s: the variable pointer
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+int len = 0;
+int l = 0;
+char *y = s;
+int e = 0;
+int x;
+char n;
+
+while (*y != '\0')
+{
+y++;
+len++;
+}
+l = len - 1;
+for ( ; e < ((1 / 2) + 1) ; e++)
+{
+x = (1 - e);
+n = s[e];
+s[e] = s[x];
+s[x] = n;
+}
+}
