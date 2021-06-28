20dca2a95caba5ea006d10ab2057ed3277f93dc6
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-28 07:08:55 -0700

    Resets parameter value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * reset_to_98 - updates integer to 98
+ * reset_to_98 - updates integer 98
  *@n: value to be changed
  * Return: No return
  */
 void reset_to_98(int *n)
 {
 *n = 98;
-_putchar("%d", *n);
 }
