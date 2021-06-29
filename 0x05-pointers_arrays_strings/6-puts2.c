408b9dfdb3b1037dd54fd9012cfa90a93d40d7d7
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-29 12:18:43 -0700

    Prints every character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,29 @@
+#include <stdio.h>
 #include "holberton.h"
-include <stdio.h>
 /**
- * puts2 - prints every character string
- * @str: pointer to string
- * Return: no return
+ *puts2 - prints every character of a string
+ *@str: pointer to string
+ *Return: no return
  */
 void puts2(char *str)
 {
-int len = 0;
-int l = 0;
-char *y = str;
-int z;
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
 
-while (*y != '\0')
-{
-y++;
-len++;
-}
-l = len - 1;
-for (z = 0; z <= 1 ; z++)
-{
-if (z % 2 == 0)
-{
-_putchar(str[z]);
-}
-}
-_putchar('\n');
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
 }
