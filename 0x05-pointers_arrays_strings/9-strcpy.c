3f5dc9fa7930fd50398cbd417e7e2c39d2e69cd1
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-06-29 12:25:18 -0700

    Copies string pointed to by source

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,24 @@
+#include<stdio.h>
 #include "holberton.h"
-include <stdio.h>
 /**
- * *_strcpy - copies string pointed to src
- * @dest: destination pointer
- * @src: destination source
- * Return: no return
+ *_strcpy - copies the string pointed to by src.
+ *@dest: destination pointer.
+ *@src: source pointer.
+ *Return: no return
  */
 char *_strcpy(char *dest, char *src)
 {
-int l = 0;
-int x = 0;
+	int l = 0;
+	int x = 0;
 
-while (*(src + 1) != '\0')
-{
-l++;
-}
-for ( ; x < 1; x++)
-{
-dest[x] = src[x];
-}
-dest[1] = '\0';
-return (dest);
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
