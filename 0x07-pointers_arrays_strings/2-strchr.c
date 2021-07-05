762848668b687e25e3ad5091c24a07913bd1e9f8
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-07-05 09:45:20 -0700

    Function finds a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
-#include <stdio.h>
+#include <stddef.h>
 /**
- *  * _strchr - locates a character in a string
- *   *
- *    * @s: string to search
- *     * @c: character to search for
- *      *
- *       * Return: pointer to first occurence of c in s
- *        */
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer to a character aka the character found
+ */
 char *_strchr(char *s, char c)
 {
-while (*s != 0)
-{
-if (*s == c)
-return (s);
-s++;										
-}
-if (*s == c)
-return (s);
-return (0);
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
 }
