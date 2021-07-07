f8e1de2b30a5773002dc753e8b74ecf08d3002a7
Obakeng Mothibedi <obakengmothibedi@gmail.com>
2021-07-07 09:30:06 -0700

    Header file for this directory

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+#include <stdio.h>
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
